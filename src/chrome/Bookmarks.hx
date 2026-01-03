745c0e17967c0ac79ede9156db9825a9afc1b822
Kamalei Zestri <38802353+KamaleiZestri@users.noreply.github.com>
2026-01-03 01:12:48 -0600

    Update bookmarks with promises and optional callbacks

--- src/chrome/Bookmarks.hx
+++ src/chrome/Bookmarks.hx
@@ -1,8 +1,9 @@
 package chrome;
 
 import chrome.Events;
+import js.lib.Promise;
 
-@:enum abstract BookmarkTreeNodeUnmodifiable(String) from String to String{
+enum abstract BookmarkTreeNodeUnmodifiable(String) from String to String{
 	var managed = "managed";
 }
 
@@ -64,17 +65,17 @@ extern class Bookmarks {
 	static var MAX_SUSTAINED_WRITE_OPERATIONS_PER_MINUTE(default,never) : Int;
 
 	@:overload(function( ?idList:Array<String>, callback : Array<BookmarkTreeNode>->Void ) : Void {} )
-	static function get( id : String, callback : Array<BookmarkTreeNode>->Void ) : Void;
-	static function getChildren( id : String, callback : Array<BookmarkTreeNode>->Void ) : Void;
-	static function getRecent( numberOfItems : Int, callback : Array<BookmarkTreeNode>->Void ) : Void;
-	static function getTree( callback : Array<BookmarkTreeNode>->Void ) : Void;
-	static function getSubTree( id : String, callback : Array<BookmarkTreeNode>->Void ) : Void;
-	static function search( query : String, callback : Array<BookmarkTreeNode>->Void ) : Void;
-	static function create( bookmark : Bookmark, ?callback : BookmarkTreeNode->Void ) : Void;
-	static function move( id : String, destination : BookmarkDestination, ?callback : BookmarkTreeNode->Void ) : Void;
-	static function update( id : String, changes : BookmarkChanges, ?callback : Array<BookmarkTreeNode>->Void ) : Void;
-	static function remove( id : String, ?callback : Void->Void ) : Void;
-	static function removeTree( id : String, ?callback : Void->Void ) : Void;
+	static function get( id : String, ?callback : Array<BookmarkTreeNode>->Void) : Promise<Array<BookmarkTreeNode>>;
+	static function getChildren( id : String, ?callback : Array<BookmarkTreeNode>->Void ) : Promise<Array<BookmarkTreeNode>>;
+	static function getRecent( numberOfItems : Int, ?callback : Array<BookmarkTreeNode>->Void ) : Promise<Array<BookmarkTreeNode>>;
+	static function getTree( ?callback : Array<BookmarkTreeNode>->Void ) : Promise<Array<BookmarkTreeNode>>;
+	static function getSubTree( id : String, ?callback : Array<BookmarkTreeNode>->Void ) : Promise<Array<BookmarkTreeNode>>;
+	static function search( query : String, ?callback : Array<BookmarkTreeNode>->Void ) : Promise<Array<BookmarkTreeNode>>;
+	static function create( bookmark : Bookmark) : Promise<BookmarkTreeNode>;
+	static function move( id : String, destination : BookmarkDestination, ?callback : BookmarkTreeNode->Void ) : Promise<BookmarkTreeNode>;
+	static function update( id : String, changes : BookmarkChanges, ?callback : Array<BookmarkTreeNode>->Void ) : Promise<BookmarkTreeNode>;
+	static function remove( id : String, ?callback : Void->Void ) : Promise<Void>;
+	static function removeTree( id : String, ?callback : Void->Void ) : Promise<Void>;
 
 	static var onCreated(default,never) : Event<String->BookmarkTreeNode->Void>;
 	static var onRemoved(default,never) : Event<String->BookmarkRemoveInfo->Void>;
